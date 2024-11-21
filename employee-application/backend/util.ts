d60017a126bcdb6b8fa42dd87ddefbcce242c344
unknown <joona.uutela@gmail.com>
2024-11-21 10:00:15 +0200

    add amplify, remove file

--- employee-application/backend/util.ts
+++
@@ -1,6 +0,0 @@
-export const getContextPath = () => {
-  if (!process.env.CONTEXT_PATH) {
-    throw new Error("Context path required");
-  }
-  return process.env.CONTEXT_PATH;
-};
--- employee-application/frontend/src/app/components/GenericError.tsx
+++
@@ -1,9 +0,0 @@
-import { Alert } from "@mui/material";
-
-export default function GenericError() {
-  return (
-    <Alert variant="filled" severity="error">
-      Error occured. Please try again later
-    </Alert>
-  );
-}
